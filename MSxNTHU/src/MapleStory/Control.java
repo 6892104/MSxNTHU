5d55f8304c8bbe7a7073b3949a73b8285e41c88d
Rex <NTHUCS@140.114.86.204>
2016-05-30 21:15:14 +0800

    add bag image

--- MSxNTHU/src/MapleStory/Control.java
+++ MSxNTHU/src/MapleStory/Control.java
@@ -34,6 +34,16 @@ public class Control extends Thread{
 	    bag = new Bag(display, dataBase);
 	    bagDelay = 0;
 	    display.setBag(bag);
+	    /*JButton button = new JButton();
+	    button.setBorderPainted(false);
+	    button.setBorder(null);
+	    button.setFocusable(false);
+	    button.setMargin(new Insets(0, 0, 0, 0));
+	    button.setContentAreaFilled(false);
+	    button.setIcon(myIcon1);
+	    button.setRolloverIcon(myIcon2);
+	    button.setPressedIcon(myIcon3);
+	    button.setDisabledIcon(myIcon4);*/
 	}
 	
 	@Override
