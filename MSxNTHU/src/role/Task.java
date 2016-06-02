4048b4d3c10e0bbc8d39d6804e8ca4a813f43608
Rex <Rex@140.114.86.241>
2016-06-02 23:40:17 +0800

    add Task

--- MSxNTHU/src/role/Task.java
+++ MSxNTHU/src/role/Task.java
@@ -1,5 +1,80 @@
 package role;
 
-public class Task {
+import java.awt.Image;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+
+public class Task {
+	String name;
+	public Task(String name){
+		this.name = name;
+		loadData();
+	}
+	
+	private void loadData(){
+		JSONObject data;
+		JSONArray data_array;
+		try{
+			String file = "message/" + name + ".json";
+			data = new PApplet().loadJSONObject(file);
+		
+			data_array = data.getJSONArray("messages");
+			for(int i =0; i< data_array.size(); i++) {
+				String dialog = data_array.getJSONObject(i).getString("dialog");
+				String type = data_array.getJSONObject(i).getString("type");
+				String message = data_array.getJSONObject(i).getString("message");
+				if(dialog.equals("message")){
+					JOptionPane.showMessageDialog(null, message, name+" :", getPicture(type) );
+				}else if(dialog.equals("confirm")){
+					int option = JOptionPane.showConfirmDialog(null, message, name+" :", JOptionPane.YES_NO_OPTION, getPicture(type));
+					if(option == JOptionPane.YES_OPTION){
+						String yes = data_array.getJSONObject(i).getString("yes");
+						if(yes.equals("task")){
+							int number = data_array.getJSONObject(i).getInt("task_number");
+							JSONArray tasks = data.getJSONArray("tasks");
+							doTask(tasks.getJSONObject(number));
+						}
+					}
+				}
+			}
+		
+		}catch (NullPointerException ie){
+			javax.swing.JOptionPane.showMessageDialog(null, "載入" + name + "對話錯誤");
+		}
+	}
+	
+	private void doTask(JSONObject task){
+		if(task.getString("type").equals("collect")){
+			String item = task.getString("item");
+			int number = task.getInt("number");
+			int option = JOptionPane.showConfirmDialog(null, "幫我收集 "+item+" " +number+" 個好嗎？", name+" :", JOptionPane.YES_NO_OPTION, getPicture("question"));
+			/*if(option == JOptionPane.YES_OPTION){
+				String yes = data_array.getJSONObject(i).getString("yes");
+				if(yes.equals("task")){
+					int number = data_array.getJSONObject(i).getInt("task_number");
+					JSONArray tasks = data.getJSONArray("tasks");
+					doTask(tasks.getJSONObject(number));
+				}
+			}*/
+		}
+	}
+	
+	private int getPicture(String type){
+		if(type.equals("information"))
+			return JOptionPane.INFORMATION_MESSAGE;
+		else if(type.equals("question"))
+			return JOptionPane.QUESTION_MESSAGE;
+		else if(type.equals("warning"))
+			return JOptionPane.WARNING_MESSAGE;
+		else if(type.equals("error"))
+			return JOptionPane.ERROR_MESSAGE;
+		else
+			return JOptionPane.PLAIN_MESSAGE;
+	}
 }
