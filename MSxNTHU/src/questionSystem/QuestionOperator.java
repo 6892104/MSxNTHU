c145c7687f2294268b2aacabe92527354cfa0d6e
andy72039 <andy72039@gmail.com>
2016-06-05 14:41:11 +0800

    change question result to chinese discription

--- MSxNTHU/src/questionSystem/QuestionOperator.java
+++ MSxNTHU/src/questionSystem/QuestionOperator.java
@@ -11,44 +11,61 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class QuestionOperator {
-    private BufferedReader reader;
-    private BufferedWriter writer;
-    public HashMap<String, String> questions;
-    public QuestionOperator() {
-        try {
-            reader = new BufferedReader(new FileReader(new File("resource/question_list.txt")));
-            questions = new HashMap<String, String>();
-            String qTitle = reader.readLine();
-            String qContent = reader.readLine();
-            while((qTitle != null) && (qContent != null)) {
-                questions.put(qTitle, qContent);
-                writer = new BufferedWriter(new FileWriter("resource/" + qTitle + ".txt", false));
-                writer.write(qTitle + ": " + qContent + "\n");
-                writer.close();
-                qTitle = reader.readLine();
-                qContent = reader.readLine();
-            }
-            reader.close();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	private BufferedReader reader;
+	private BufferedWriter writer;
+	public HashMap<String, String> questions;
+	public QuestionOperator() {
+		try {
+			reader = new BufferedReader(new FileReader(new File("resource/question_list.txt")));
+			questions = new HashMap<String, String>();
+			String qTitle = reader.readLine();
+			String qContent = reader.readLine();
+			while((qTitle != null) && (qContent != null)) {
+				questions.put(qTitle, qContent);
+				writer = new BufferedWriter(new FileWriter("resource/" + qTitle + ".txt", false));
+				writer.write(qTitle + ": " + qContent + "\n");
+				writer.close();
+				qTitle = reader.readLine();
+				qContent = reader.readLine();
+			}
+			reader.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public String getQuestion(String fileName) {
-    	return questions.get(fileName);
-    }
+	public String getQuestion(String fileName) {
+		return questions.get(fileName);
+	}
 
-    public void writeAnswer(String fileName, String inputAns) {
-    	if(inputAns.equals("0"))
-    		return;
-        try {
-        	System.out.println(inputAns);
-            writer = new BufferedWriter(new FileWriter("resource/" + fileName + ".txt", true));
-            writer.append(inputAns + "\n");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public void writeAnswer(String fileName, String inputAns) {
+		String result;
+		switch(inputAns) {
+		case "0" :
+			return;
+			break;
+		case "1":
+			result = "非常不同意";
+			break;
+		case "2":
+			result = "不同意";
+			break;
+		case "3":
+			result = "同意";
+			break;
+		case "4":
+			result = "非常同意";
+			break;
+		default:
+			return;
+		}
+		try {
+			writer = new BufferedWriter(new FileWriter("resource/" + fileName + ".txt", true));
+			writer.append(result + "\n");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
